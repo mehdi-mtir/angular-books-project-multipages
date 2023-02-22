39cda9f8f795277f416cf595c20850f8669bae6c
mehdi-mtir <mehdi.mtir@gmail.com>
2023-02-22 13:07:47 +0100

    Ajout d'un livre

--- src/app/books/add-books/add-books.component.ts
+++ src/app/books/add-books/add-books.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/models/book';
+import { BooksService } from 'src/app/services/books.service';
 
 @Component({
   selector: 'app-add-books',
@@ -7,7 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBooksComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private bookService : BooksService,
+    private router : Router) { }
+
+  addBook(f : NgForm){
+    //console.log(f);
+    const newBook = new Book(
+      this.bookService.getLastId() + 1,
+      f.value.titre,
+      f.value.auteur,
+      f.value.prix
+    )
+    this.bookService.addBook(newBook);
+    this.router.navigate(['/books']);
+  }
 
   ngOnInit(): void {
   }
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { ListBooksComponent } from './list-books/list-books.component';
 import { AddBooksComponent } from './add-books/add-books.component';
 import { EditBooksComponent } from './edit-books/edit-books.component';
-import { RouterModule } from '@angular/router';
 
 
 
@@ -15,7 +17,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ]
 })
 export class BooksModule { }
